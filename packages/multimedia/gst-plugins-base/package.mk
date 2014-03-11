8beeda752ff9212c1a5222a270058fbadda02ea9
Michael Taubert <michael@arachnodroid.de>
2014-03-11 06:24:54 +0100

    - switched gst-plugins-base from git master to 1.2.3

--- packages/multimedia/gst-plugins-base/package.mk
+++ packages/multimedia/gst-plugins-base/package.mk
@@ -17,133 +17,28 @@
 ################################################################################
 
 PKG_NAME="gst-plugins-base"
-PKG_VERSION="master"
+PKG_VERSION="1.2.3"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
 PKG_SITE="http://gstreamer.freedesktop.org/gstreamer"
-#PKG_URL="http://gstreamer.freedesktop.org/src/gst-plugins-base/$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_URL="http://gstreamer.freedesktop.org/src/$PKG_NAME/$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_DEPENDS_TARGET="toolchain libpng tiff dbus fontconfig eglibc zlib gstreamer"
 PKG_PRIORITY="optional"
 PKG_SECTION="lib"
-PKG_SHORTDESC="gstreamer plugins base"
-PKG_LONGDESC="gstreamer plugins base"
+PKG_SHORTDESC="gst-plugins-base"
+PKG_LONGDESC="gst-plugins-base"
 
 PKG_IS_ADDON="no"
-PKG_AUTORECONF="no"
+PKG_AUTORECONF="yes"
 
-case $TARGET_ARCH in
-	i386)
-		PKG_DEPENDS_TARGET="gstreamer libvorbis"
-		PKG_BUILD_DEPENDS_TARGET="gstreamer libvorbis"
-	;;
-	x86_64)
-		PKG_DEPENDS_TARGET="gstreamer libvorbis"
-		PKG_BUILD_DEPENDS_TARGET="gstreamer libvorbis"
-	;;
-	arm)
-		PKG_DEPENDS_TARGET="gstreamer libvorbis"
-		PKG_BUILD_DEPENDS_TARGET="gstreamer libvorbis"
-	;;
-esac
+if [ "$XBMCPLAYER_DRIVER" = "bcm2835-driver" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET bcm2835-driver"
+fi
 
-case $PROJECT in
-	Generic)
-		PKG_CONFIGURE_OPTS="
-				--prefix=/usr \
-				--disable-maintainer-mode \
-				--disable-dependency-tracking \
-				--disable-silent-rules \
-				--disable-failing-tests \
-				--disable-fatal-warnings \
-				--disable-examples"
-	;;
-	RPi)
-		PKG_CONFIGURE_OPTS="\
-			--prefix=/usr \
-			-target=arm-none-linux-gnueabi \
-			-host=arm-none-linux-gnueabi \
-			--disable-maintainer-mode \
-			--disable-dependency-tracking \
-			--disable-silent-rules \
-			--disable-failing-tests \
-			--disable-fatal-warnings \
-			--disable-x \
-			--disable-xshm \
-			--disable-pango \
-			--disable-alsa \
-			--disable-examples"
-	;;
-esac
-
-
-unpack() {
-
-	if [ ! -e $SOURCES/$1 ]; then
-		mkdir -p $SOURCES/$1
-	fi
-	
-	pushd $SOURCES/$1/
-	
-	if [ ! -e ${PKG_NAME}-${PKG_VERSION} ]; then
-		git clone git://anongit.freedesktop.org/gstreamer/gst-plugins-base gst-plugins-base-master
-	fi
-	
-	if [ ! -e $ROOT/$BUILD/${PKG_NAME}-${PKG_VERSION} ]; then
-		cp -r ${PKG_NAME}-${PKG_VERSION} $ROOT/$BUILD/
-	fi
-	
-	popd
-}
-
-configure_target() {
-
-	pushd ${ROOT}/${BUILD}/${PKG_NAME}-${PKG_VERSION}
-
-	if [ ! -e configure ]; then
-		./autogen.sh --noconfigure
-	fi
-
-	case $PROJECT in
-		Generic)
-			./configure ${PKG_CONFIGURE_OPTS}
-		;;
-		RPi)
-			./configure ${PKG_CONFIGURE_OPTS}
-		;;
-	esac
-	
-	popd
-}
-
-#make_target() {
-#	case $PROJECT in
-#		Generic)
-#		;;
-#		RPi)
-#		;;
-#	esac
-#}
-
-#makeinstall_target() {
-#	case $PROJECT in
-#		Generic)
-#		;;
-#		RPi)
-#		;;
-#	esac
-#}
-
-#pre_install() {
-#	makeinstall_target
-#}
-
-post_install() {
-	case $PROJECT in
-		Generic)
-		;;
-		RPi)
-			cp -R $SYSROOT_PREFIX/usr/lib/gstreamer-1.0/* $INSTALL/usr/lib/gstreamer-1.0/
-		;;
-	esac
-}
-
+PKG_CONFIGURE_OPTS_TARGET="\
+		--disable-maintainer-mode \
+		--disable-dependency-tracking \
+        --disable-silent-rules \
+        --disable-fatal-warnings \
+		--disable-static"
